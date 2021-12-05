dd4ec6333d7be19b2b341b0a17476cf4d8ac08fc
levar <maciej.rybacki@twoupdigital.com>
2021-12-05 14:06:26 +0100

    Poprawiono strukturę

--- 1/test/test_Car.py
+++ 1/test/test_Car.py
@@ -1,37 +1,5 @@
 import unittest
 
-
-class Car:
-    def __init__(self, brand, model, engine, price=0, is_electric=False):
-        self.brand = brand
-        self.model = model
-        self.engine = engine
-        self.price = price
-        self.is_electric = is_electric
-
-    def get_brand(self):
-        return self.brand
-
-    def get_model(self):
-        return self.model
-
-    def get_engine(self):
-        return self.engine
-
-    def get_price(self):
-        if self.price > 0:
-            return self.price
-        else:
-            raise ValueError("Price should be greather than 0")
-
-
-    def get_is_electric(self):
-        if self.engine == "electric":
-            return True
-        else:
-            return False
-
-
 class CarTestCase(unittest.TestCase):
     def setUp(self):
         self.electric_car = Car(brand="Tesla", model="X", engine="electric", price=50000)
